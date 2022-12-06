87588b347727878e422f01720cc1fc10d9b8fa08
Harris Hanozidis <harris2310@hotmail.com>
2022-12-06 18:27:16 +0200

    More setup  and created fetch to API

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Navbar from "./layouts/Navbar";
+import "./styles/App.css";
 
 function App() {
+  useEffect(() => {
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=20")
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Navbar />
+      Pokedex
     </div>
   );
 }
 
 export default App;
+
