195ee035af4bf045d6717a43c2079c9736b0f261
Harris Hanozidis <harris2310@hotmail.com>
2022-12-08 15:38:47 +0200

    Changed loading css

--- README.md
+++ README.md
@@ -7,9 +7,22 @@ My Pokedex App for the Linked Buisness interview
 Clone the repository locally, delete package-lock and npm install to install the necessary modules\
 Then npm start
 
+## How to use
+
+Search on the input bar a pokemon you like and check it's details\
+On the pokemon details click on an evolution to see the details of it's evolution\
+Press randomize to check out randomly selected pokemons\
+
+## Things To Fix
+
+Disentangle component logic\
+Maybe split logic from UI\
+Add context\
+Testing\
+
 ## To-Do
 
-Search by type\
+Search by multiple types - Change from select to something with multiple choice\
 Load More\
 Local Storage\
 Responsive\
@@ -17,8 +30,11 @@ Responsive\
 ## Done
 
 Ολα τα required\
+Search by type\
 CNTRL + F\
 Escape\
+Loading...\
 Somewhat responsive\
 Random Pokemon\
 Some animations
+
--- src/layouts/Navbar.tsx
+++ src/layouts/Navbar.tsx
@@ -7,7 +7,7 @@ type Props = { handlePokemonChange: Function; handleLogoClick: (e: any) => void 
 
 const Navbar = ({ handlePokemonChange, handleLogoClick }: Props) => {
   const [value, setValue] = useState<string>("");
-  const [types, setTypes] = useState([]);
+  const [types, setTypes] = useState<Array<string>>([]);
   const [open, setOpen] = useState(false);
   const [fetchError, setFetchError] = useState(false);
   const focused = useRef<any>(false);
@@ -27,6 +27,7 @@ const Navbar = ({ handlePokemonChange, handleLogoClick }: Props) => {
     }
   };
   const handleSearchSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    // Αν δεν εχει τιμη το input επεστρεψε τα αρχικα 15
     e.preventDefault();
     if (value == "") {
       const result = await fetchPokemonInit();
@@ -38,8 +39,8 @@ const Navbar = ({ handlePokemonChange, handleLogoClick }: Props) => {
         .catch((err) => setFetchError(true));
     }
   };
-  const handleDropdown = (e: any) => {
-    setOpen(!open);
+  const handleTypeSelect = (e: any) => {
+    setTypes([e.target.value]);
   };
   return (
     <div className='navbar-main'>
@@ -53,15 +54,15 @@ const Navbar = ({ handlePokemonChange, handleLogoClick }: Props) => {
         </button>
       </form>
       <div>
-        <select name='type' title='types' className='dropdown-button' onClick={handleDropdown}>
-          <option value='any'>Any Type</option>
+        <select name='type' title='types' className='dropdown-button' onChange={handleTypeSelect}>
+          <option value='0'>Any Type</option>
           <option value='7'>Bug</option>
-          <option value='saab'>Dragon</option> <option value='18'>Fairy</option> <option value='10'>Fire</option> <option value='8'>Ghost</option> <option value='5'>Ground</option>{" "}
-          <option value='1'>Normal</option> <option value='saab'>Psychic</option> <option value='9'>Steel</option> <option value='saab'>Dark</option> <option value='13'>Electric</option>{" "}
+          <option value='16'>Dragon</option> <option value='18'>Fairy</option> <option value='10'>Fire</option> <option value='8'>Ghost</option> <option value='5'>Ground</option>{" "}
+          <option value='1'>Normal</option> <option value='14'>Psychic</option> <option value='9'>Steel</option> <option value='17'>Dark</option> <option value='13'>Electric</option>{" "}
           <option value='2'>Fighting</option>
           <option value='3'>Flying</option>
           <option value='12'>Grass</option>
-          <option value='saab'>Ice</option>
+          <option value='15'>Ice</option>
           <option value='4'>Poison</option>
           <option value='6'>Rock</option>
           <option value='11'>Water</option>
