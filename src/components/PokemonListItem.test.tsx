53aaa26b41218bce575b857d6f5df0740cc76cfb
Harris Hanozidis <harris2310@hotmail.com>
2022-12-20 21:03:28 +0200

    e stoppropagation not functioning in tests

--- src/components/PokemonListItem.test.tsx
+++ src/components/PokemonListItem.test.tsx
@@ -1,7 +1,8 @@
 /* eslint-disable no-restricted-globals */
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen, fireEvent, createEvent } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import "@testing-library/jest-dom";
+import { prettyDOM } from "@testing-library/react";
 import PokemonListItem from "./PokemonListItem";
 import { GlobalContext } from "../context/GlobalContext";
 
@@ -16,23 +17,46 @@ let pok = {
   stats: { base_stat: "HP", effort: 0, stat: { name: "HP", url: "..." } },
 };
 
-const mock = { open: false, handleOpen: jest.fn(), handleClose: () => {}, handleFavouriteToggle: () => {}, favouriteClass: "item-favourite-white" };
+const mock = {
+  open: false,
+  handleOpen: jest.fn(),
+  handleClose: () => {},
+  handleFavouriteToggle: jest.fn((e: any) => {
+    e.stopPropagation();
+  }),
+  favouriteClass: "item-favourite-white",
+};
 
 jest.mock("../containers/usePokemonListItem", () => {
   return () => mock;
 });
 
-describe("Component should render if favourites is empty", () => {
-  beforeAll(() => {});
-  test("The test", () => {
+describe("Component clicks should trigger specific handlers", () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+  test("Star click shold trigger handleFavouriteToggle only", () => {
     const { container } = render(
       <GlobalContext.Provider value={{ pokemon: [], setPokemon: () => {}, favourites: [], setFavourites: () => {}, loading: false, setLoading: () => {} }}>
         <PokemonListItem pok={pok} />
       </GlobalContext.Provider>,
     );
-    fireEvent.click(screen.getByText("★"));
-    fireEvent.click(screen.getByText("★"));
+    const clickEvent = createEvent.click(screen.getByText("★"));
+
+    fireEvent(screen.getByText("★"), clickEvent);
+    expect(mock.handleFavouriteToggle.mock.calls.length).toBe(1);
+    // expect(mock.handleOpen).not.toHaveBeenCalled(); // DOESN"T PASS, NEED e.stopPropagation()
+    expect(container).toHaveTextContent("★Snorlax");
+  });
+  test("Button but not star click should trigger handleOpen only", () => {
+    const { container } = render(
+      <GlobalContext.Provider value={{ pokemon: [], setPokemon: () => {}, favourites: [], setFavourites: () => {}, loading: false, setLoading: () => {} }}>
+        <PokemonListItem pok={pok} />
+      </GlobalContext.Provider>,
+    );
+    fireEvent.click(screen.getByRole("img"));
     expect(mock.handleOpen).toHaveBeenCalled();
+    expect(mock.handleFavouriteToggle).not.toHaveBeenCalled();
     expect(container).toHaveTextContent("★Snorlax");
   });
 });
